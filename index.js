6beba185f987528c2fe93db9e6130473feeec10f
NexyusNex <shootdietale@gmail.com>
2023-01-27 18:27:03 +0100

    Added functionality for the cats button

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const image = document.querySelector("#image");
+const cats = document.querySelector("#cats");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=eiCCVwTsJ7uWKA9Hyc9TZ8Wosb2FzmhW&s=cats",
-  {
+function searchImage(url) {
+  fetch(url, {
     mode: "cors",
-  }
-)
-  .then((response) => {
-    return response.json();
   })
-  .then((response) => {
-    image.src = response.data.images.original.url;
-  });
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      image.src = response.data.images.original.url;
+    });
+}
+
+cats.addEventListener("click", () => {
+  const url =
+    "https://api.giphy.com/v1/gifs/translate?api_key=eiCCVwTsJ7uWKA9Hyc9TZ8Wosb2FzmhW&s=cats";
+  searchImage(url);
+});
