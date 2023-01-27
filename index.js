537fd4c2c66d17e246a37225f6dd1d3344bfa600
NexyusNex <shootdietale@gmail.com>
2023-01-27 18:40:26 +0100

    Added Search functionality and an error if the input field is empty

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const image = document.querySelector("#image");
 const cats = document.querySelector("#cats");
+const search = document.querySelector("#search");
 
 function searchImage(url) {
   fetch(url, {
@@ -16,5 +17,19 @@ function searchImage(url) {
 cats.addEventListener("click", () => {
   const url =
     "https://api.giphy.com/v1/gifs/translate?api_key=eiCCVwTsJ7uWKA9Hyc9TZ8Wosb2FzmhW&s=cats";
+  searchImage(url);
+});
+
+search.addEventListener("click", () => {
+  const input = document.querySelector("#text");
+  const error = document.querySelector(".error");
+  if (input.value == "") {
+    error.textContent = "Search can't be empty!";
+    return;
+  }
+  error.textContent = "";
+  const url =
+    "https://api.giphy.com/v1/gifs/translate?api_key=eiCCVwTsJ7uWKA9Hyc9TZ8Wosb2FzmhW&s=" +
+    input.value;
   searchImage(url);
 });
